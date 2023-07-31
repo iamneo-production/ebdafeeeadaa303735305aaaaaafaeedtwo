c542d657be6ccd29cab170d22ba017a84f25bcd1
Iamneo <techadmin@iamneo.ai>
2023-07-31 12:25:50 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/LaptopController.java
+++ springapp/src/main/java/com/examly/springapp/controller/LaptopController.java
@@ -5,7 +5,6 @@ import com.examly.springapp.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/")
